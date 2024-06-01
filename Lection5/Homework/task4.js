c7ec5984bab10a81348dd3ff719ef40b5dcbd61b
Dmitry Neichev <dima.neychev@simplerfid.com>
2024-06-01 13:19:24 +0300

    #3. Lection 5 homework

---
+++ Lection5/Homework/task4.js
@@ -0,0 +1,29 @@
+// Дано ціле число (ввести через 'prompt'). З'ясувати, чи просто воно (простим називається число, більше 1, що не має інших дільників, крім 1 і себе).
+
+function showTask4() {
+  let userNum = parseInt(prompt("Enter Num"));
+
+  if (isNaN(userNum)) {
+    console.log("Value is NaN");
+  } else {
+    if (isPrime(userNum)) {
+      console.log(userNum + " is prime.");
+    } else {
+      console.log(userNum + " is not prime.");
+    }
+  }
+}
+
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
