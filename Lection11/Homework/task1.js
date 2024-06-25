c0742c2617165f846f321afc671945b1615d7c74
Dmitry Neichev <dima.neychev@simplerfid.com>
2024-06-25 17:26:33 +0300

    #11. Lection 11 homework

---
+++ Lection11/Homework/task1.js
@@ -0,0 +1,29 @@
+// Вивести таблицю Піфагора (10×10), таблиця повинна бути створена динамічно
+
+// Елемент sandbox береться із main.js;
+
+function showTask1() {
+  sandbox.append(generatePythagorasTable(10));
+}
+
+function generatePythagorasTable(size) {
+  const table = document.createElement("table")
+  const tableBody = document.createElement("tbody");
+  table.classList.add('pythagoras-table');
+
+  for (let x = 1; x <= size; x++) {
+    const row = document.createElement("tr");
+
+    for (let y = 1; y <= size; y++) {
+      const cell = document.createElement("td");
+      cell.textContent = x * y;
+      row.append(cell);
+    }
+
+    tableBody.append(row);
+  }
+
+  table.append(tableBody);
+
+  return table;
+}
